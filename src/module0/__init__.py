0531ab89aa4da16e0d88aba5122cc319698527b3
Nate Nichols <nathannichols454@gmail.com>
2024-12-28 18:11:34 -0500

    initial

---
+++ src/module0/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 2024-12-28T18:11:34-05:00
+
+@author: nate
+"""
+
+from . import util
---
+++ src/module0/util.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 2024-12-28T18:11:34-05:00
+
+@author: nate
+"""
+
+
+def replace_me():
+    print(__name__)
